d62d8924b4fe132ccc367d60c3723cf22e083578
Kamil <kdnuriev@gmail.com>
2025-07-13 03:02:32 +0400

    skdjfkasfd

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-#Pass version using command line argument
-command sudo docker build --no-cache -t create-video-note-bot:$1 .
-command sudo docker run -d -e TOKEN=8032716817:AAE0JEthFKACO3ey_EWSrtSLmfBkOchZQdA create-video-note-bot:$1
+#Pass token using command line argument
+command sudo docker build --no-cache -t create-video-note-bot .
+command sudo docker run -d -e TOKEN=$1 create-video-note-bot
